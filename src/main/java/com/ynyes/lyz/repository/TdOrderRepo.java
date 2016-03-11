802e6518ca2ff73d44c62907ca0da6628fb33585
Administrator <Administrator@IT-20150912DWGD>
2016-03-11 16:42:13 +0800

    行政街道添加城市

--- src/main/java/com/ynyes/lyz/repository/TdOrderRepo.java
+++ src/main/java/com/ynyes/lyz/repository/TdOrderRepo.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.ynyes.lyz.entity.TdOrder;
