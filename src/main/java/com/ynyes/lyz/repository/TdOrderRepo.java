cd2fbe7f4b6f5c035c9d28b8d2f276afbfced2e3
Administrator <Administrator@IT-20150912DWGD>
2016-03-11 16:44:32 +0800

    导入jar包

--- src/main/java/com/ynyes/lyz/repository/TdOrderRepo.java
+++ src/main/java/com/ynyes/lyz/repository/TdOrderRepo.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.ynyes.lyz.entity.TdOrder;
