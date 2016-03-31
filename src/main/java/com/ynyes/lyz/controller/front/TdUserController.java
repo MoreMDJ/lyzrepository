fc9d00186121f9cf607406b2783fcc00001d51d3
邓潇 <894559632@qq.com>
2016-03-31 09:28:09 +0800

    154号问题解决

--- src/main/java/com/ynyes/lyz/controller/front/TdUserController.java
+++ src/main/java/com/ynyes/lyz/controller/front/TdUserController.java
@@ -247,12 +247,10 @@ public class TdUserController {
 
 			// 查找所有待评价的订单
 			List<TdOrder> uncomment_order_list = tdOrderService.findByUsernameAndStatusId(username, 5L);
-			map.addAttribute("user_type",0);
+			map.addAttribute("user_type", 0);
 			map.addAttribute("uncomment_order_list", uncomment_order_list);
-		} 
-		else if (1L == user.getUserType().longValue()) 
-		{
-			
+		} else if (1L == user.getUserType().longValue()) {
+
 			// 查询所有的归属销顾为自己的订单
 			Page<TdOrder> all_order_page = tdOrderService.findBySellerIdAndStatusIdNotOrderByOrderTimeDesc(user.getId(),
 					0, 20);
@@ -274,12 +272,11 @@ public class TdUserController {
 			map.addAttribute("unsignin_order_list", unsignin_order_list);
 
 			// 查找所有未评价的订单
-			List<TdOrder> uncomment_order_list = tdOrderService.findBySellerIdAndStatusIdOrderByOrderTimeDesc(user.getId(), 5L);
-			map.addAttribute("user_type",1);
+			List<TdOrder> uncomment_order_list = tdOrderService
+					.findBySellerIdAndStatusIdOrderByOrderTimeDesc(user.getId(), 5L);
+			map.addAttribute("user_type", 1);
 			map.addAttribute("uncomment_order_list", uncomment_order_list);
-		}
-		else if (2L == user.getUserType().longValue()) 
-		{
+		} else if (2L == user.getUserType().longValue()) {
 			// 获取用户的门店
 			TdDiySite diySite = tdCommonService.getDiySite(req);
 			if (null != diySite) {
@@ -302,7 +299,7 @@ public class TdUserController {
 				// 获取门店所有未评价的订单
 				List<TdOrder> uncomment_order_list = tdOrderService
 						.findByDiySiteIdAndStatusIdOrderByOrderTimeDesc(diySite.getId(), 5L);
-				map.addAttribute("user_type",2);
+				map.addAttribute("user_type", 2);
 				map.addAttribute("uncomment_order_list", uncomment_order_list);
 			}
 		}
@@ -1317,6 +1314,12 @@ public class TdUserController {
 
 		// 查询到指定的订单
 		TdOrder order = tdOrderService.findOne(orderId);
+		if (null != order.getStatusId()) {
+			if (2L == order.getStatusId().longValue() && 3L == order.getStatusId().longValue()) {
+				res.put("message", "已出库的订单不能取消");
+				return res;
+			}
+		}
 		// 首先判断订单是不是运费单
 		String orderNumber = order.getOrderNumber();
 		String newOrderNumber = "";
