12747c897660dafa4662407d0d5f9646b767f96b
madejing <Administrator@PC-20150701TNZB>
2016-04-05 10:05:25 +0800

    修改支付宝回调为正式服务器ip

--- src/main/java/com/ynyes/lyz/util/SiteMagConstant.java
+++ src/main/java/com/ynyes/lyz/util/SiteMagConstant.java
@@ -29,7 +29,7 @@ public class SiteMagConstant {
 		if(operation.contains("Linux")){
 			backupPath = "/mnt/root/backup/";
 			imagePath = "/mnt/root/images/goods";
-			alipayReturnUrl = "http://123.57.32.143:8080/pay/alipay/return";
+			alipayReturnUrl = "http://101.200.128.65:8080/pay/alipay/return";
 		}else{
 			backupPath = "src/main/resources/backup/";
 			imagePath = "src/main/resources/static/images";
